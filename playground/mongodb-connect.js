c66631752693d39fb188fa8b8bb0a46dc4404e2f
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-06-23 12:33:45 -0400

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to mongoDB server');
+    }
+    console.log('Connected to mongoDB server');
+    
+    // const db = client.collection('Todoes').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert Todo', err);
+            
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+        
+    // });
+
+    const db = client.collection('Users').insertOne({
+        name: 'Joel',
+        age: 29,
+        location: 'florida'
+    }, (err, result) => {
+        if (err) {
+            console.log('Unable to insert new user', err);
+            
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+        
+    });
+
+    client.close();
+});
