b60e0446c567566979af9e34bb8fa6e2f9056b48
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-07-02 13:44:22 -0400

    stupid typo

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-cont port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
