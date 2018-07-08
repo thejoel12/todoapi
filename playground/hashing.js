aea283a556c4508b723aa37d969f28ca6af718c1
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-07-08 10:46:50 -0400

    has userpassword before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,19 +1,36 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123asd';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+        
+//     })
+// });
+
+var hashedPassword = '$2a$10$vBoQCYHWrbhW2HLG3a5A8ui9OV0iQY4TZykCKUVMacrSgYgxVZjzS';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+    
+});
+
+// var data = {
+//     id: 10
+// };
 
 
-var token = jwt.sign(data, '123asd');
+// var token = jwt.sign(data, '123asd');
 
-console.log(token);
+// console.log(token);
 
 
 
-var decoded = jwt.verify(token, '123asd');
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, '123asd');
+// console.log('decoded', decoded);
 
 
 
@@ -45,3 +62,4 @@ console.log('decoded', decoded);
 //     console.log('Data was changed, do not trust');
     
 // }
+
